87d636bdee6fcdc8168e8d97b1f78fa51f571b31
belaab <izabela150b@gmail.com>
2018-04-06 23:51:38 +0200

    .gitignore file added

---
+++ GeoXplore/GeoXplore/New Group/ChooseSignUpInViewController.swift
@@ -0,0 +1,35 @@
+//
+//  ChooseSignUpInViewController.swift
+//  GeoXplore
+//
+//  Created by Izabela Brzeczek on 06.04.2018.
+//  Copyright © 2018 Izabela Brzeczek. All rights reserved.
+//
+
+import UIKit
+
+class ChooseSignUpInViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
