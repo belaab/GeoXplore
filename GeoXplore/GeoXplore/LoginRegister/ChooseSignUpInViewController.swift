bd203cf8356a9b221e96723828ec57662c5d945b
belaab <izabela150b@gmail.com>
2018-04-07 02:10:09 +0200

    basic login UI v2

---
+++ GeoXplore/GeoXplore/LoginRegister/ChooseSignUpInViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ChooseSignUpInViewController.swift
+//  GeoXplore
+//
+//  Created by Izabela Brzeczek on 06.04.2018.
+//  Copyright © 2018 Izabela Brzeczek. All rights reserved.
+//
+
+import UIKit
+
+class ChooseSignUpInViewController: UIViewController {
+    
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+ 
+
+}
